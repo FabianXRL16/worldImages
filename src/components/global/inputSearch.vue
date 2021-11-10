b2562d7367d2d7118e94cb2764ff6a902e6e316b
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-10 17:15:05 -0500

    Input for search created

---
+++ src/components/global/inputSearch.vue
@@ -0,0 +1,29 @@
+<template>
+    <input type="text" placeholder="To Search image!">
+</template>
+
+<script>
+export default {
+  name: "inputSearch",
+  props: {},
+};
+</script>
+
+<style scoped>
+input {
+  background-color: white;
+  width: 100%;
+  height: 50px;
+  border-radius: 25px;
+  outline: none;
+  border: none;
+  float: left;
+  padding: 0 55px 0 20px;
+  box-sizing: border-box;
+  font-size: 20px;
+  color: #21d3b5;
+}
+input::placeholder{
+    color: #21d3b5af;
+}
+</style>
