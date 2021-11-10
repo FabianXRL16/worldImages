f1d3448f7aad9852cc6f5359dc23d9102046fa03
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-10 17:15:15 -0500

    Header custom

---
+++ src/components/custom/wiHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "wiHeader",
+  props: {},
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #21d3b5;
+  width: 100%;
+  height: 60px;
+}
+</style>
