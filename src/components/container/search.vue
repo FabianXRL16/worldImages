4067e66141071724732260f72566aeaa536be310
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-10 17:15:34 -0500

    Container of search

---
+++ src/components/container/search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="search">
+    <input-search />
+    <btn-base @hideMsg="hideText">
+      <template v-slot:icon>
+        <i class="fas fa-search"></i>
+      </template>
+    </btn-base>
+  </div>
+</template>
+
+<script>
+import inputSearch from "../global/inputSearch.vue";
+import btnBase from "../global/btnBase.vue";
+export default {
+  name: "search",
+  props: {},
+  components: {
+    inputSearch,
+    btnBase,
+  },
+  methods:{
+      hideText(){
+          this.$emit('noShowTitle')
+      }
+  }
+};
+</script>
+
+<style scoped>
+.search {
+  width: 100%;
+  height: 50px;
+  position: relative;
+}
+</style>
