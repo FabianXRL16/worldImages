738fb1d87049bd1d6a96ee7a7257916a5aea0af6
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-10 17:14:18 -0500

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <wiHeader />
     <router-view/>
   </div>
 </template>
 
+<script>
+import wiHeader from './components/custom/wiHeader.vue';
+export default {
+  name: "App",
+  components:{
+    wiHeader
+  }
+};
+</script>
+
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;500;600&display=swap');
+body{
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  font-family: 'Poppins', sans-serif;
+}
+:root{
+  --bg-primary: #21d3b5;
+  --bg-primary-dark: #106e5f;
+  --bg-primary-light: #21d3b565;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  color: #ffffff;
 }
 </style>
