eb3f9ee3e95e29169003257c892721713262850a
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-10 18:40:30 -0500

    Add logo

--- src/components/custom/wiHeader.vue
+++ src/components/custom/wiHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="header">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/" class="logo">
+      <img src="../../assets/logo.svg" alt="">
+      <span>Fab</span>
+    </router-link>
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </header>
 </template>
@@ -15,7 +19,28 @@ export default {
 <style scoped>
 header {
   background-color: #21d3b5;
-  width: 100%;
+  width: 100vw;
   height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+a{
+  text-decoration: none;
+  color: #fff;
+}
+.logo{
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+.logo img{
+  height: 40px;
+}
+.logo span{
+  font-size: 35px;
+  line-height: 35px;
 }
 </style>
