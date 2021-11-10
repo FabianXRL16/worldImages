be9719f4100b00459fe2ae78f1dfaa3afef32153
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-10 17:14:51 -0500

    View home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1 class="msg">A buscar!</h1>
+      <search @noShowTitle='hideTitle' />
+      <images />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import search from "../components/container/search.vue";
+import images from "../components/container/cards.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    search, images
+  },
+  methods:{
+    hideTitle(){
+      let $title = document.querySelector('.msg');
+      let $container = document.querySelector('.container');
+      let $containerImages = document.querySelector('.containerImages');
+      $title.style.display = 'none';
+      $container.style.justifyContent = 'flex-start'
+      $containerImages.style.display = 'grid'
+    }
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  background-color: #21d3b565;
+  width: 100%;
+  height: calc(100vh - 60px);
+  box-sizing: border-box;
+  display: grid;
+  place-content: center;
+}
+.container {
+  height: calc(100vh - 200px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+}
+.msg {
+  margin: -50px 0 0;
+  font-size: 60px;
+  line-height: 60px;
+}
+@media all and (min-width: 960px) {
+  .container {
+    width: 60vw;
+    gap: 50px;
+  }
+  .msg {
+    margin: -50px 0 0;
+    font-size: 75px;
   }
 }
-</script>
+@media all and (min-width: 768px) and (max-width: 959px) {
+  .container {
+    width: 70vw;
+  }
+}
+@media all and (min-width: 480px) and (max-width: 767px) {
+  .container {
+    width: 85vw;
+  }
+}
+@media all and (max-width: 479px) {
+  .container {
+    width: 90vw;
+  }
+  .msg {
+    font-size: 50px;
+    line-height: 50px;
+  }
+}
+</style>
