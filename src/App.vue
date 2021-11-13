b1a67aefe4069b1f8f79b457fa6f47394635b537
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-13 01:25:42 -0500

    Add modal msg

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div id="app">
     <wiHeader />
-    <router-view/>
+    <router-view />
     <modal-image v-if="$store.state.modal" />
+    <modal-msg />
   </div>
 </template>
 
 <script>
-import wiHeader from './components/custom/wiHeader.vue';
-import modalImage from './components/container/modalContainer.vue';
+import wiHeader from "./components/custom/wiHeader.vue";
+import modalImage from "./components/container/modalContainer.vue";
+import modalMsg from "./components/container/modalMsg.vue";
 export default {
   name: "App",
-  components:{
-    wiHeader, modalImage
-  }
+  components: {
+    wiHeader,
+    modalMsg,
+    modalImage,
+  },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;500;600&display=swap');
-body{
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;500;600&display=swap");
+body {
   margin: 0;
   width: 100%;
   height: 100vh;
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
-:root{
+:root {
   --bg-primary: #21d3b5;
   --bg-primary-dark: #106e5f;
   --bg-primary-light: #21d3b565;
