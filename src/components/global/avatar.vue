141319c80217a72c5951f41fbc93df6360c182e3
Fabian Pacherres <fabianpacherres@gmail.com>
2021-11-14 19:48:30 -0500

    Avatar dinamic

--- src/components/global/avatar.vue
+++ src/components/global/avatar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="avatar">
-      <h2>Fabián</h2>
-      <img src="../../assets/user.jpg" alt="">
+    <!-- <h2>{{ $store.state._user.name.split(' ').slice(0,1).join(' ') }}</h2> -->
+    <h2>Fabian</h2>
+    <img src="../../assets/user.jpg" alt="" />
   </div>
 </template>
 
@@ -9,30 +10,33 @@
 export default {
   name: "avatar",
   props: {},
+  created() {
+    this.$store.dispatch("getUserAlegra");
+  },
 };
 </script>
 
 <style scoped>
-.avatar{
-    width: auto;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    gap: 15px;
+.avatar {
+  width: auto;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 15px;
 }
-h2{
-    margin: 0;
-    font-size: 24px;
+h2 {
+  margin: 0;
+  font-size: 24px;
 }
-img{
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-    border: 3px solid #fff;
+img {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 3px solid #fff;
 }
 @media all and (max-width: 479px) {
-  .avatar{
+  .avatar {
     gap: 10px;
-}
+  }
 }
 </style>
